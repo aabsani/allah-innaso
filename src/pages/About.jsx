12d138057208d5b08a97b057a789a6db8a741129
Abdulwahab Sani <abdulwsani@gmail.com>
2025-05-25 10:47:57 +0100

    Adjusted Layout
    Wrapped component in a class "content-pages" to avoid overlapping with navbar

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,19 +2,36 @@ import React from "react";
 
 export default function About() {
   return (
-    <section className="about">
-      <h2>About Us</h2>
-      <p>
-        At Allah Inna’so Nigeria Limited, we specialize in the supply and
-        distribution of high-quality frozen foods to retail outlets, hospitality
-        establishments, and individual customers.
-      </p>
-      <h3>Our Team</h3>
-      <ul>
-        <li>Managing Director</li>
-        <li>Sales Manager</li>
-        <li>Legal</li>
-      </ul>
-    </section>
+    <div className="content-page">
+      <section className="about">
+        <h2>About Us</h2>
+        <p>
+          At Allah Inna'so Nigeria Limited, we specialize in the supply and
+          distribution of high-quality frozen foods to retail outlets,
+          hospitality establishments, and individual customers.
+        </p>
+        <div className="statement-cards">
+          <div className="mission">
+            <h3>Mission Statement:</h3>
+            <p>
+              To provide our customers with premium-quality, health-conscious
+              frozen food products through reliable supply chains, exceptional
+              service, and a steadfast commitment to food safety and excellence.
+            </p>
+          </div>
+          <div className="vision">
+            <h3>Vision Statement:</h3>
+            <p>
+              To become the leading provider of healthy and premium frozen
+              foods, recognized for quality, integrity, and innovation in food
+              distribution across Nigeria and beyond.
+            </p>
+          </div>
+        </div>
+        <h3>Our Team</h3>
+        <p>Alh. (Dr.) Muhammed Salifu Abdullahi</p>
+        <p>MD/CHIEF EXECUTIVE OFFICER</p>
+      </section>
+    </div>
   );
 }
