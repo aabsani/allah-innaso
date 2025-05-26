2f77a667c00297c4d5d9e45211d113a0bba67527
Abdulwahab Sani <abdulwsani@gmail.com>
2025-05-26 17:28:11 +0100

    Updated Data.js

--- data.js
+++ data.js
@@ -1,10 +1,79 @@
 export const teamMembers = [
   {
     id: 1,
-    image: "/images/team/member1.jpg",
+    image: "/images/team/salifu-abdullahi.jpeg",
     name: "Alh. (Dr.) Muhammed Salifu Abdullahi",
     title: "MD/CHIEF EXECUTIVE OFFICER",
-    description: "Leadership and strategic direction...",
+    description:
+      "With a strong background in Political Science and Policy Analysis, Dr. Salifu brings strategic leadership and deep analytical skills to the food supply industry. He holds a Ph.D. in Political Science with a specialization in Policy Analysis, an M.Sc. in Political Science and Diplomacy and a B.Sc. in Political Science. His academic journey has equipped him with a comprehensive understanding of policy development, organizational strategy and global diplomatic frameworks. All of which contribute to his effective leadership in the highly competitive frozen food sector. Under his direction, Allah Inna'so Nigeria Limited has expanded its market reach, strengthened supply chains and maintained a reputation for quality and reliability. His visionary leadership continues to drive the company towards innovation, sustainability, and customer satisfaction.",
   },
-  { id: 2, image: "" },
+  {
+    id: 2,
+    image: "/images/team/hassan-abdulkarim.jpeg",
+    name: "Hassan Abdulkarim",
+    title: "General Manager",
+    description:
+      "Serves as the General Manager of Allah Inna'so Nigeria Limited, a leading supplier of frozen foods. With a solid background in finance and economics. Mall. Abdulkarim brings valuable insight and strategic direction to the company's operations. He holds a Diploma in Accounting and Finance as well as a Bachelor of Science (B.Sc) degree in Economics. With five years of experience in the industry, he has played a pivotal role in overseeing the company’s growth, ensuring operational efficiency, and maintaining strong relationships with partners and clients.",
+  },
+  {
+    id: 3,
+    image: "/images/team/yakubu-abdulwahab.jpeg",
+    name: "Yakubu Abdulwahab",
+    title: "Operational Manager",
+    description:
+      "With a strong background in educational management and administrative coordination, Mr. Abdulwahab brings strategic oversight and operational efficiency to the company's day-to-day processes. He holds a Nigeria Certificate in Education (NCE) in English and Political Science as well as a Bachelor’s degree in Educational Management. His experience and leadership ensure seamless operations across the supply chain, contributing to the company’s commitment to quality and timely delivery.",
+  },
+  {
+    id: 4,
+    image: "/images/team/emmanuel-agbemre.jpeg",
+    name: "Amb. Emmanuel Agbemre",
+    title: "CSO/Regional Manager",
+    description:
+      "A Security Expert with a strong background in both physical and information security, he oversees the protection of company assets, infrastructure and operations across the region. He began his academic journey with a National Certificate in Education (NCE) in Agricultural Science (Double Major) and later earned a Bachelor’s degree in Education (B.Ed), specializing in Guidance and Counselling. Driven by a passion for security and risk management in the digital age, he is currently pursuing a Postgraduate Diploma (PGD) in Cybersecurity. Amb. Agbemre brings a unique blend of educational insight, practical leadership, and technical knowledge to his dual role, ensuring both the physical safety and cyber integrity of the company’s operations.",
+  },
+  {
+    id: 5,
+    image: "/images/team/yusuf-alih.jpeg",
+    name: "Yusuf Alih Yusuf",
+    title: "Marketing Manager",
+    description:
+      "Leads the development and execution of marketing strategies that is aimed at expanding the company’s market presence and driving sales growth in the frozen foods sector. Mall. Yusuf's role includes overseeing branding, customer engagement, market research, and promotional campaigns that align with the company’s vision and goals. He holds a Higher National Diploma (HND) in Business Administration and Management.",
+  },
+  {
+    id: 6,
+    image: "/images/team/zubair-hassan.jpeg",
+    name: "Bar. Zubair Hassan",
+    title: "H.R./Legal Officer",
+    description:
+      "A young brilliant Lawyer with a strong foundation in both legal practice and organisational management. He serves as the Human Resource and Legal Officer. In this dual capacity, Bar. Zubair manages employee relations, recruitment and policy development while also responsible for providing legal guidance, ensuring regulatory compliance and managing all legal matters related to the company’s operations in the frozen foods industry. Bar. Zubair holds a Bachelor of Laws (LL.B Double Hons.) as well as a Barrister-at-Law (B.L) qualification from the Nigerian Law School, which equips him with a comprehensive understanding of the procedural aspects of the law.",
+  },
+  {
+    id: 7,
+    image: "/images/team/shuaibu-ibrahim.jpeg",
+    name: "Shuaibu Ibrahim",
+    title: "Facility Supervisor",
+    description:
+      "Oversees the day-to-day operations, maintenance, and efficiency of the company’s cold storage and distribution facilities. With a strong focus on quality assurance, safety, and operational excellence, he plays a vital role in ensuring that all frozen food products are stored and handled under optimal conditions. Mall. Ibrahim holds a Bachelor of Science (B.Sc) degree in Political Science bringing a unique perspective to facility management through strong organizational, analytical, and leadership skills.",
+  },
+  {
+    id: 8,
+    image: "/images/team/salifu-wada.jpeg",
+    name: "Mr. Salifu Wada Odaudu",
+    title: "LOGISTICS MANAGER",
+    description: "",
+  },
+  {
+    id: 9,
+    image: "/images/team/oyetunde-akeem.jpeg",
+    name: "Mr. Oyetunde Akeem",
+    title: "Area Manager",
+    description: "",
+  },
+  {
+    id: 10,
+    image: "/images/team/elizabeth-ishaku.jpeg",
+    name: "Miss. Elizabeth Ishaku",
+    title: "Accountant",
+    description: "",
+  },
 ];
