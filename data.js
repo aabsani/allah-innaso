e62a2108a3aa774ae3f707c089246081b71307b9
Abdulwahab Sani <abdulwsani@gmail.com>
2025-05-26 15:37:22 +0100

    Created Data.js
    Data for team members with respective descriptions

---
+++ data.js
@@ -0,0 +1,10 @@
+export const teamMembers = [
+  {
+    id: 1,
+    image: "/images/team/member1.jpg",
+    name: "Alh. (Dr.) Muhammed Salifu Abdullahi",
+    title: "MD/CHIEF EXECUTIVE OFFICER",
+    description: "Leadership and strategic direction...",
+  },
+  { id: 2, image: "" },
+];
