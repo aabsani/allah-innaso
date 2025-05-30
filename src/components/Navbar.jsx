20b58bfc1ce5911ddedc8b457a54b1dc4e5dde5c
Abdulwahab Sani <abdulwsani@gmail.com>
2025-05-30 21:39:26 +0100

    Added Responsiveness

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import { useState, useEffect } from "react";
-import { FaFacebookF, FaInstagram, FaXTwitter, FaBars, FaTimes } from "react-icons/fa6";
+import { FaFacebookF, FaInstagram, FaXTwitter, FaBars } from "react-icons/fa6";
 import { NavLink } from "react-router-dom";
 
 export default function Navbar() {
@@ -26,28 +26,44 @@ export default function Navbar() {
   };
 
   return (
-    <nav className={`navbar ${scrolled ? "navbar-scrolled" : ""} ${mobileMenuOpen ? "mobile-menu-open" : ""}`}>
+    <nav
+      className={`navbar ${scrolled ? "navbar-scrolled" : ""} ${
+        mobileMenuOpen ? "mobile-menu-open" : ""
+      }`}
+    >
       <NavLink to="/">
         <img src="/images/logo-1.jpeg" alt="logo" className="logo" />
       </NavLink>
-      
+
       <button className="mobile-menu-button" onClick={toggleMobileMenu}>
-        {mobileMenuOpen ? <FaTimes /> : <FaBars />}
+        {/* {mobileMenuOpen ? <FaTimes /> : <FaBars />} */}
       </button>
 
       <ul className={mobileMenuOpen ? "show-mobile-menu" : ""}>
         <li>
-          <NavLink to="/" className="nav-item" onClick={() => setMobileMenuOpen(false)}>
+          <NavLink
+            to="/"
+            className="nav-item"
+            onClick={() => setMobileMenuOpen(false)}
+          >
             Home
           </NavLink>
         </li>
         <li>
-          <NavLink to="/about" className="nav-item" onClick={() => setMobileMenuOpen(false)}>
+          <NavLink
+            to="/about"
+            className="nav-item"
+            onClick={() => setMobileMenuOpen(false)}
+          >
             About Us
           </NavLink>
         </li>
         <li>
-          <NavLink to="/services" className="nav-item" onClick={() => setMobileMenuOpen(false)}>
+          <NavLink
+            to="/services"
+            className="nav-item"
+            onClick={() => setMobileMenuOpen(false)}
+          >
             Services
           </NavLink>
         </li>
@@ -57,12 +73,16 @@ export default function Navbar() {
           </NavLink>
         </li> */}
         <li>
-          <NavLink to="/contact" className="nav-item" onClick={() => setMobileMenuOpen(false)}>
+          <NavLink
+            to="/contact"
+            className="nav-item"
+            onClick={() => setMobileMenuOpen(false)}
+          >
             Contact Us
           </NavLink>
         </li>
       </ul>
-      
+
       <div className="social-icons">
         <a
           href="https://facebook.com"
